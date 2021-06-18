453e4b26460da6841e101953745e914431619134
Vandesm14 <vandesm14@gmail.com>
2021-06-18 17:56:19 -0400

    make the site

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,73 @@
 
 </script>
 
-<h1>Welcome to Fridge Sales dot Info!</h1>
+<table>
+	<th colspan="3">
+		<center>
+			<a href="https://www.statista.com/topics/2182/refrigerators-and-freezers/">
+				<h1>Fridge Sales this Year</h1>
+			</a>
+		</center>
+	</th>
+	<tr>
+		<td>
+			<h1>Market Size</h1>
+		</td>
+		<td>
+			<h1>106.7bn USD</h1>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h1>Unit Sales</h1>
+		</td>
+		<td>
+			<h1>193.4 million</h1>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h1>Whirlpool Net Sales</h1>
+		</td>
+		<td>
+			<h1>6.06bn USD</h1>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3">
+			<img src="/fav.svg" alt="fridge">
+		</td>
+	</tr>
+</table>
 
-<h2></h2>
+<footer>
+	Made with Fridge by <a href="https://twitter.com/Vandesm14">Shane</a>
+</footer>
 
 <style>
+table {
+	width: 60%;
+	margin: 6% auto;
+}
 
+td, th {
+	padding: 1rem 2rem;
+	border: 3px solid grey;
+}
+
+h1 {
+	width: max-content;
+}
+
+img {
+	display: block;
+	width: 80%;
+	margin: auto;
+}
+
+footer {
+	font-size: 1.6rem;
+	text-align: center;
+	margin-bottom: 1rem;
+}
 </style>
